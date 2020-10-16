edf55a5b33629b152da9642d6c97ca3f75c5015f
Romain-Martinez <70898022+Romain-Martinez@users.noreply.github.com>
2020-10-16 14:22:32 +0200

    Delete e03.js

--- e03.js
+++
@@ -1,9 +0,0 @@
-function SortInverse (newArray, firstIndex, secondIndex){
-    let sort = newArray[firstIndex]
-    newArray[firstIndex] = newArray[secondIndex];
-    newArray[secondIndex] = sort
-}
-function sortBySelection(newArray){
-    var length = newArray.length,
-    
-}
