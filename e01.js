611dd075eda370f8e3a7c8be1a78ae639b635698
Romain.Martinez <romainmartinez250@gmail.com>
2020-10-16 11:15:14 +0200

    .\e01.js

---
+++ e01.js
@@ -0,0 +1,14 @@
+const tri =(array)=>{
+    for(let i =0; i < array.length ; i++){
+        while(!array.sort()){
+            if(array[i] > array[i + 1]){
+                first = array[i];
+                array[i] = array[i + 1]
+                first = array[i + 1]
+            }
+        }
+    }
+    return array
+}
+
+console.log(tri([66,10,88]))
